a80cabcc6aeeff45356373e5f369c6906ef196f5
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2021-12-08 16:32:32 +0700

    Insert Query For Product Tables

---
+++ Query/insert product.sql
@@ -0,0 +1,94 @@
+INSERT INTO product VALUES ('MA1G','Makanan','Goreng','Mie Ayam Original',10000);
+INSERT INTO product VALUES ('MA1K','Makanan','Kuah','Mie Ayam Original',10000);
+
+INSERT INTO product VALUES ('MA2G','Makanan','Goreng','Mie Ayam Jumbo',16000);
+INSERT INTO product VALUES ('MA2K','Makanan','Kuah','Mie Ayam Jumbo',16000);
+
+INSERT INTO product VALUES ('MA3G','Makanan','Goreng','Mie Ayam Super Jumbo Komplit',16000);
+INSERT INTO product VALUES ('MA3K','Makanan','Kuah','Mie Ayam Super Jumbo Komplit',16000);
+
+INSERT INTO product VALUES ('BK1U','Makanan','Urat','Bakso Hore',12000);
+INSERT INTO product VALUES ('BK1H','Makanan','Halus','Bakso Hore',12000);
+INSERT INTO product VALUES ('BK1C','Makanan','Campur','Bakso Hore',12000);
+
+INSERT INTO product VALUES ('BK2U','Makanan','Urat','Bakso Yummie',15000);
+INSERT INTO product VALUES ('BK2H','Makanan','Halus','Bakso Yummie',15000);
+INSERT INTO product VALUES ('BK2C','Makanan','Campur','Bakso Yummie',15000);
+
+INSERT INTO product VALUES ('BK3U','Makanan','Urat','Bakso Happy',18000);
+INSERT INTO product VALUES ('BK3H','Makanan','Halus','Bakso Happy',18000);
+INSERT INTO product VALUES ('BK3C','Makanan','Campur','Bakso Happy',18000);
+
+INSERT INTO product VALUES ('BK4U','Makanan','Urat','Bakso Judez',17000);
+INSERT INTO product VALUES ('BK4H','Makanan','Halus','Bakso Judez',17000);
+INSERT INTO product VALUES ('BK4C','Makanan','Campur','Bakso Judez',17000);
+
+INSERT INTO product VALUES ('BK5U','Makanan','Urat','Bakso Jumbo Tidak Pedas',17000);
+INSERT INTO product VALUES ('BK5UH','Makanan','Halus','Bakso Jumbo Tidak Pedas',17000);
+
+INSERT INTO product VALUES ('BK6U','Makanan','Urat','Bakso Jumbo Isian Tetelan',20000);
+INSERT INTO product VALUES ('BK6H','Makanan','Halus','Bakso Jumbo Isian Tetelan',20000);
+INSERT INTO product VALUES ('BK6C','Makanan','Campur','Bakso Jumbo Isian Tetelan',20000);
+
+INSERT INTO product VALUES ('BK7U','Makanan','Urat','Bakso Bagong',35000);
+INSERT INTO product VALUES ('BK7H','Makanan','Halus','Bakso Bagong',35000);
+INSERT INTO product VALUES ('BK7C','Makanan','Campur','Bakso Bagong',35000);
+
+INSERT INTO product VALUES ('BB1P','Makanan','Pedas','Bakso Bakar Hore',14000);
+INSERT INTO product VALUES ('BB1M','Makanan','Manis','Bakso Bakar Hore',14000);
+
+INSERT INTO product VALUES ('BB2P','Makanan','Pedas','Bakso Bakar Yummie',17000);
+INSERT INTO product VALUES ('BB2M','Makanan','Manis','Bakso Bakar Yummie',17000);
+
+INSERT INTO product VALUES ('ST1','Makanan','Soto','Nasi Soto Daging Sapi',10000);
+INSERT INTO product VALUES ('ST2','Makanan','Soto','Soto Daging Sapi(Tanpa Nasi)',10000);
+INSERT INTO product VALUES ('ST3','Makanan','Soto','Nasi Soto Ayam',10000);
+INSERT INTO product VALUES ('ST4','Makanan','Soto','Soto Ayam(Tanpa Nasi)',8000);
+
+--Minuman
+INSERT INTO product VALUES ('EO1','Minuman','Es','Es Teler Palgading',10000);
+INSERT INTO product VALUES ('EO2','Minuman','Es','Es Campur Gula Jawa',10000);
+INSERT INTO product VALUES ('EO3','Minuman','Es','Es Pelangi',10000);
+INSERT INTO product VALUES ('EO4','Minuman','Es','Es Manado',10000);
+INSERT INTO product VALUES ('EO5','Minuman','Es','Es Cendol Pelangi',10000);
+INSERT INTO product VALUES ('EO6','Minuman','Es','Es Cincau Hitam',8000);
+INSERT INTO product VALUES ('EO7','Minuman','Es','Es Capcin(Capucino Cincau)',8000);
+INSERT INTO product VALUES ('EO8','Minuman','Es','Es Micin(Milo Cincau)',8000);
+INSERT INTO product VALUES ('EO9','Minuman','Es','Es Milkshake Oreo',10000);
+INSERT INTO product VALUES ('EO10','Minuman','Es','Es Milkshake Oreo',10000);
+INSERT INTO product VALUES ('EO11E','Minuman','Es','Es/Hot Milo',6000);
+INSERT INTO product VALUES ('EO11H','Minuman','Hot','Es/Hot Milo',6000);
+INSERT INTO product VALUES ('EO12E','Minuman','Es','Es/Hot Capucino',6000);
+INSERT INTO product VALUES ('EO12H','Minuman','Hot','Es/Hot Capucino',6000);
+INSERT INTO product VALUES ('EO13E','Minuman','Es','Es/Hot Chocolate',6000);
+INSERT INTO product VALUES ('EO13H','Minuman','Hot','Es/Hot Chocolate',6000);
+INSERT INTO product VALUES ('EO14E','Minuman','Es','Es/Hot Teh',4000);
+INSERT INTO product VALUES ('EO14H','Minuman','Hot','Es/Hot Teh',4000);
+INSERT INTO product VALUES ('EO15E','Minuman','Es','Es/Hot Jeruk',5000);
+INSERT INTO product VALUES ('EO15H','Minuman','Hot','Es/Hot Jeruk',5000);
+INSERT INTO product VALUES ('EO16','Minuman','Es','Es Sirup',5000);
+INSERT INTO product VALUES ('EO17','Minuman','Es','Es Saparela',12000);
+INSERT INTO product VALUES ('EO18','Minuman','Es','Es Soda Gembira',12000);
+INSERT INTO product VALUES ('EO19','Minuman','Es','Kopi Hitam',5000);
+
+INSERT INTO product VALUES ('ED1','Minuman','Es','Es Teller Durian',14000);
+INSERT INTO product VALUES ('ED2','Minuman','Es','Es Campur Gula Jawa Durian',14000);
+INSERT INTO product VALUES ('ED3','Minuman','Es','Es Pelangi Durian',14000);
+INSERT INTO product VALUES ('ED4','Minuman','Es','Es Degan Durian',14000);
+INSERT INTO product VALUES ('ED5','Minuman','Es','Es Alpukat Durian',14000);
+INSERT INTO product VALUES ('ED6','Minuman','Es','Es Alpukat Degan Durian',14000);
+INSERT INTO product VALUES ('ED7','Minuman','Es','Es Cincau Hitam Durian',14000);
+
+INSERT INTO product VALUES ('EJ1','Minuman','Es','Juice Melon',8000);
+INSERT INTO product VALUES ('EJ2','Minuman','Es','Juice Mangga',8000);
+INSERT INTO product VALUES ('EJ3','Minuman','Es','Juice Alpukat',10000);
+INSERT INTO product VALUES ('EJ4','Minuman','Es','Juice Semangka',18000);
+INSERT INTO product VALUES ('EJ5','Minuman','Es','Juice Jambu Merah',8000);
+INSERT INTO product VALUES ('EJ6','Minuman','Es','Juice Sirsat',8000);
+INSERT INTO product VALUES ('EJ7','Minuman','Es','Juice Durian',10000);
+INSERT INTO product VALUES ('EJ8','Minuman','Es','Juice Jeruk',8000);
+
+--Jajanan
+INSERT INTO product VALUES ('JJ1','Jajanan','Jajanan','French Fries(Kentang Goreng)',10000);
+INSERT INTO product VALUES ('JJ2','Jajanan','Jajanan','Tela-Telo',10000);
+INSERT INTO product VALUES ('JJ3','Jajanan','Jajanan','Batagor',10000);
