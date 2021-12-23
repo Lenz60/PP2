217d258f17f3ff892b7dde73bfa87c130cdda87a
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2021-12-23 17:45:46 +0700

    Apicall Order is succesfully tested
    Apicall for Order is now Succesfully tested and ready to go
    
    TODO :
    - Implement on App

--- Api/api.php
+++ Api/api.php
@@ -13,7 +13,7 @@
 					$notable = $_POST['notable']; 
 					$guestname = $_POST['guestname']; 
 
-					$stmt = $conn->prepare("INSERT INTO Guest(Kode_meja,No_meja,Nama_guest) VALUES(?,?,?)");
+						$stmt = $conn->prepare("INSERT INTO Guest(Kode_meja,No_meja,Nama_guest) VALUES(?,?,?)");
 						$stmt->bind_param("sss", $tablecode, $notable, $guestname);
 						$stmt->execute();
 
@@ -62,11 +62,55 @@
 					$toppingcode8 = $_POST['toppingcode8'];
 					$toppingcode9 = $_POST['toppingcode9'];
 					$toppingcode10 = $_POST['toppingcode10'];
+
+					// //Select Price from DB
+					// $stmt = $conn->prepare("SELECT Harga_produk FROM product WHERE Kode_produk = ? ");
+					// $stmt->bind_param("s",$productcode);
+					// $stmt->execute();
+					// $stmt->store_result();
+					// if($stmt->num_rows > 0){
+					// 	$stmt->bind_result($productPrice);
+					// 	$ProductPriceFinal = $productPrice * $productqty;
+
+					// 	$response['error'] = false;
+					// 	$response['message'] = 'Succesfully declared Price as local variable';
+					// }
+					// else{
+					// 	$response['error'] = true;
+					// 	$response['message'] = 'Failed declared Price as local variable';
+					// }
+
+					//Order Query (All Null excetp Code Order)
+					$stmt = $conn->prepare("INSERT INTO Orders(Kode_order,Status_order) VALUES(?,'Disiapkan')");
+					$stmt->bind_param("s", $ordercode);
+					$stmt->execute();
 					
 					//Guest Query (Insert Guest Query)
 					$stmt = $conn->prepare("INSERT INTO Guest_Order(Kode_Order,Kode_Meja) VALUES(?,?)");
-					$stmt->bind_param("ss",$ordercode,$notable);
+					$stmt->bind_param("ss", $ordercode, $tablecode);
 					$stmt->execute();
+
+					//Guest Query (Set Guest Code to Local Variable)
+					$stmt = $conn->prepare("SELECT Kode_Guest_Order FROM guest_order WHERE Kode_Order = ? ");
+					$stmt->bind_param("s",$ordercode);
+					$stmt->execute();
+					$stmt->store_result();
+
+					if($stmt->num_rows > 0){
+						$stmt->bind_result($guestcodeorder);
+						$stmt->fetch();
+						// echo "guestcodeorder = ".$guestcode;
+
+						$response['error'] = false;
+						$response['message'] = 'Succesfully declared Guest as local variable';
+						
+					}
+					else{
+						$response['error'] = true;
+						$response['message'] = 'Failed declared Guest as local variable';
+					}
+					// // echo "guestcodeorder 2 = ".$guestcode;
+
 
 					//Product Query (Insert Product Order)
 					$stmt = $conn->prepare("INSERT INTO Product_Order(Kode_Produk,Kode_Order,Jumlah_Produk_PO) VALUES(?,?,?)");
@@ -81,15 +125,18 @@
 
 					if($stmt->num_rows > 0){
 						$stmt->bind_result($productordercode);
-						$ProductOrderCode = $productordercode;
+						$stmt->fetch();
+						//  echo "productordercode = ".$productordercode;
 
 						$response['error'] = false;
 						$response['message'] = 'Succesfully declared Product as local variable';
+						
 					}
 					else{
 						$response['error'] = true;
 						$response['message'] = 'Failed declared Product as local variable';
 					}
+					// echo "productordercode2 = ".$productordercode;
 
 					//Query Topping (Insert Topping Order)
 						//Topping 1
@@ -98,7 +145,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode1);
+						$stmt->bind_param("ss",$productordercode,$toppingcode1);
 						$stmt->execute();
 					}
 						//Topping 2
@@ -107,7 +154,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode2);
+						$stmt->bind_param("ss",$productordercode,$toppingcode2);
 						$stmt->execute();
 					}
 						//Topping 3
@@ -116,7 +163,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode3);
+						$stmt->bind_param("ss",$productordercode,$toppingcode3);
 						$stmt->execute();
 					}
 						//Topping 4
@@ -125,7 +172,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode4);
+						$stmt->bind_param("ss",$productordercode,$toppingcode4);
 						$stmt->execute();
 					}
 						//Topping 5
@@ -134,7 +181,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode5);
+						$stmt->bind_param("ss",$productordercode,$toppingcode5);
 						$stmt->execute();
 					}
 						//Topping 6
@@ -143,7 +190,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode6);
+						$stmt->bind_param("ss",$productordercode,$toppingcode6);
 						$stmt->execute();
 					}
 						//Topping 7
@@ -152,7 +199,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode7);
+						$stmt->bind_param("ss",$productordercode,$toppingcode7);
 						$stmt->execute();
 					}
 						//Topping 8
@@ -161,7 +208,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode8);
+						$stmt->bind_param("ss",$productordercode,$toppingcode8);
 						$stmt->execute();
 					}
 						//Topping 9
@@ -170,7 +217,7 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode9);
+						$stmt->bind_param("ss",$productordercode,$toppingcode9);
 						$stmt->execute();
 					}
 						//Topping 10
@@ -179,33 +226,19 @@
 					}
 					else {
 						$stmt = $conn->prepare("INSERT INTO Topping_Order(Kode_Produk_Order,Kode_Topping) VALUES(?,?)");
-						$stmt->bind_param("ss",$ProductOrderCode,$toppingcode10);
+						$stmt->bind_param("ss",$productordercode,$toppingcode10);
 						$stmt->execute();
 					}
 					
-					//Guest Query (Set Guest Code to Local Variable)
-					$stmt = $conn->prepare("SELECT Kode_Guest_Order FROM Guest_Order WHERE Kode_Order = ? ");
-					$stmt->bind_param("s",$ordercode);
-					$stmt->execute();
-					$stmt->store_result();
+					
+					// ///////////////////////////////////////////////////
+					// 	// ORDER QUERY HERE//
+					// //////////////////////////////////////////////////
 
-					if($stmt->num_rows > 0){
-						$stmt->bind_result($guestcodeorder);
-						$GuestCodeOrder = $guestcodeorder;
-
-						$response['error'] = false;
-						$response['message'] = 'Succesfully declared Guest as local variable';
-					}
-					else{
-						$response['error'] = true;
-						$response['message'] = 'Failed declared Guest as local variable';
-					}
-					/////////////////////////////////////////////////////
-						//ORDER QUERY HERE//
-					////////////////////////////////////////////////////
-
-					$stmt = $conn->prepare("INSERT INTO Orders(Kode_order,Kode_Guest_Order,Kode_Produk_Order,Status_order) VALUES(?,?,?,'Disiapkan')");
-					$stmt->bind_param("sss",$ordercode,$GuestCodeOrder,$ProductOrderCode);
+					$stmt = $conn->prepare("UPDATE Orders
+											SET Kode_Guest_Order = ? ,Kode_Produk_Order = ?
+											WHERE Kode_order = ?");
+					$stmt->bind_param("sss",$guestcodeorder,$productordercode,$ordercode);
 					$stmt->execute();
 				}
 				else{
