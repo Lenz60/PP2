3006b2c52f3c1ab37cb780b3f1b1b44e0cd24b22
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2021-12-27 17:06:42 +0700

    Changes on Php
    Color Code Changes :
    - Green Table color for incoming order
    - White for available table
    Table Changes :
    - Added Tipe Makanan
    - Added Total Bayar
    Backend :
    - Added some tips

--- Admin/Web/a.php
+++ Admin/Web/a.php
@@ -20,58 +20,141 @@
         </tr>
         <tr>
             <?php 
-                $table1 = "Menunggu";
-                if ($table1 == "Menunggu"){
+                $table1 = "Disiapkan";
+                $table2 = "Disiapkan";
+                $table3 = "Disiapkan";
+                $table4 = "Disiapkan";
+                $table6 = "Disiapkan";
+                $table7 = "Disiapkan";
+                $table9 = "Disiapkan";
+                $table10 = "Disiapkan";
+                $table11 = "Disiapkan";
+                $table12 = "Disiapkan";
+
+                //|Table 1
+                if ($table1 == "Disiapkan"){
                     $table1Status = "green";
                 }
-                else if ($table1 == "Disiapkan"){
-                    $table1Status = "yellow";
+                else {
+                    $table1Status = "white";
+                }
+                //|Table 2
+                if ($table2 == "Disiapkan"){
+                    $table2Status = "green";
                 }
                 else {
-                    $table1Status = "red";
+                    $table2Status = "white";
+                }
+                //|Table 3
+                if ($table3 == "Disiapkan"){
+                    $table3Status = "green";
+                }
+                else {
+                    $table3Status = "white";
+                }
+                //|Table 4
+                if ($table4 == "Disiapkan"){
+                    $table4Status = "green";
+                }
+                else {
+                    $table4Status = "white";
+                }
+                //|Table 6
+                if ($table6 == "Disiapkan"){
+                    $table6Status = "green";
+                }
+                else {
+                    $table6Status = "white";
+                }
+                //|Table 7
+                if ($table7 == "Disiapkan"){
+                    $table7Status = "green";
+                }
+                else {
+                    $table7Status = "white";
+                }
+                //|Table 9
+                if ($table9 == "Disiapkan"){
+                    $table9Status = "green";
+                }
+                else {
+                    $table9Status = "white";
+                }
+                //|Table 10
+                if ($table10 == "Disiapkan"){
+                    $table10Status = "green";
+                }
+                else {
+                    $table10Status = "white";
+                }
+                //|Table 11
+                if ($table11 == "Disiapkan"){
+                    $table11Status = "green";
+                }
+                else {
+                    $table11Status = "white";
+                }
+                //|Table 12
+                if ($table12 == "Disiapkan"){
+                    $table12Status = "green";
+                }
+                else {
+                    $table12Status = "white";
                 }
                 //? Continue
             
             
             ?>
             <tr>
-                <td colspan="4" style="height: 50px; font-size:30px;" bgcolor="<?php echo $table1Status ?>" ><a href="google.com">1</a></td>
+                <td colspan="4" style="height: 50px; font-size:30px;" bgcolor="<?php echo $table1Status ?>" ><a href="#">1</a></td>
             </tr>
 
             <tr>
-                <td style="width: 100px;height: 70px;font-size:30px;">10</td>
-                <td style="width: 100px;height: 150px;font-size:30px;" rowspan="2">9</td>
-                <td style="width: 100px;height: 150px;font-size:30px;" rowspan="2">6</td>
-                <td style="width: 100px;height: 70px;font-size:30px;">2</td>
+                <td class="sizetables1" bgcolor="<?php echo $table10Status ?>"><a href="#">10</a></td>
+                <td class="sizetables2" rowspan="2" bgcolor="<?php echo $table9Status ?>"><a href="#">9</a></td>
+                <td class="sizetables2" rowspan="2" bgcolor="<?php echo $table6Status ?>"><a href="#">6</a></td>
+                <td class="sizetables1" bgcolor="<?php echo $table2Status ?>"><a href="#">2</a></td>
             </tr>
 
-            <tr style="width: 100px;height: 70px;font-size:30px;">
-                <td>11</td>
-                <td>3</td>
+            <tr class="sizetables1">
+                <td bgcolor="<?php echo $table11Status ?>"><a href="#">11</a></td>
+                <td bgcolor="<?php echo $table3Status ?>"><a href="#">3</a></td>
             </tr>
 
-            <tr style="width: 100px;height: 70px;font-size:30px;">
-                <td>12</td>
+            <tr class="sizetables1">
+                <td bgcolor="<?php echo $table12Status ?>"><a href="#">12</a></td>
                 <td></td>
-                <td>7</td>
-                <td>4</td>
+                <td bgcolor="<?php echo $table7Status ?>"><a href="#">7</a></td>
+                <td bgcolor="<?php echo $table4Status ?>"><a href="#">4</a></td>
             </tr>
         </tr>
     </table>
     <table border="1" style="text-align: center;">
             <?php 
-                //! Determine Colspan for how many topping of certain order
-                $a = 3; 
+            //! Query 1 HERE and Store to $OrderData
+            //! Use Passed No Table from HREF POST
+                //* Determine Colspan for how many topping of certain order
+                $a = 3; //* Topping
                 $a1 = $a+1;
-                $status = true;
+                //* Check Status
+                //! Get Status Value in  $OrderData
+                //* if (echo $OrderData['status'] == "Disiapkan"){
+                //*   $status = false; //HREF Dibuat Available
+                //* }
+                //* else {
+                //*    $status = true; //HREF Selesai Available
+                //* }
+                $status = false; 
             ?>
             
             <tr>
-                <th colspan="5">Status Order</th>
+                <th colspan="7">Status Order</th>
                 <tr>
                     <th>Pesanan</th>
-                    <th>Kode Order</th>
+                    <th>Nama Makanan</th>
+                    <th>Tipe Makanan</th>
                     <th>Topping</th>
+                    <th>Total Bayar</th>
                     <th>Status</th>
                     <th>Konfirmasi Pesanan</th>
                 </tr>
@@ -81,19 +164,29 @@
                 </tr>
 
                 <tr>
-                    <td>Kode Order</td> <!-- //? Fill with OrderCode Data// -->
-                    <td>Topping1</td> <!-- //? Fill with Topping Data// -->
+                     <!-- //? Fill with OrderCode Data// -->
+                    <!-- //! Use Query 1 and use Passed No Table from Href POST-->
+                    <td rowspan="<?php echo $a ?>">Nama Makanan</td>
+                    <!-- //? Fill with OrderCode Data// -->
+                    <!-- //! Use Query 1 and use Passed No Table from Href POST-->
+                    <td rowspan="<?php echo $a ?>">Tipe Makanan</td> 
+                    <!-- //? Fill with Topping Data// -->
+                    <!-- //! Use Query 2 and use the stored Kode order to use the WHERE Clause// -->
+                    <td>Topping1</td> 
+                    <td rowspan="<?php echo $a ?>">Total Bayar</td>
                     <td rowspan="<?php echo $a ?>">Status</td>
                     <?php 
                         if ($status == true) {
                             //!Set Status to Selesai if this Clicked
                             //!Static Query Here
+                            //!USE HREF POST
                             ?><td ><a href="google.com">Selesai</a></td>
                             <?php
                         }
                         else {
                             //!Set Status to Dibuat if this Clicked
                             //!Static Query Here
+                            //!USE HREF POST
                             ?><td><a href="google.com">Dibuat</a></td>
                             <?php
                         }
