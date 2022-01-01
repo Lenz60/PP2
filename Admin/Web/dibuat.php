385e788d3d697b1ee5eb7bfade55301d0c9c0b78
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-01 14:28:09 +0700

    Status Updated Backend
    Contains query for update Status

---
+++ Admin/Web/dibuat.php
@@ -0,0 +1,12 @@
+<?php 
+include("connect.php");
+
+
+$KodeOrder =  $_POST['KodeOrder'];
+$stmt = $conn->prepare("UPDATE orders
+                        SET Status_order = 'Selesai'
+                        WHERE Kode_order = '".$KodeOrder."'");
+$stmt->execute();
+include("a.php");
+
+?>
