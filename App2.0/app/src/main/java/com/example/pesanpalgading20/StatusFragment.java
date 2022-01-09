b92e738f55877d7ca1e80fe60695f4263303006b
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-10 01:50:20 +0700

    Inflate the inflater

--- App2.0/app/src/main/java/com/example/pesanpalgading20/StatusFragment.java
+++ App2.0/app/src/main/java/com/example/pesanpalgading20/StatusFragment.java
@@ -58,7 +58,9 @@ public class StatusFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View viewRoot = inflater.inflate(R.layout.fragment_status, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        return viewRoot;
     }
 }
