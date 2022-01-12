925fac51e2fbfb7bc620786b420dcbfe68857d81
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-12 21:58:31 +0700

    Test Purposes

---
+++ App2.0/app/src/main/java/com/example/pesanpalgading20/Getter/Status/TotalFinalPrice.java
@@ -0,0 +1,22 @@
+package com.example.pesanpalgading20.Getter.Status;
+
+public class TotalFinalPrice {
+
+    private Integer TotalFinalPrice;
+
+    public TotalFinalPrice(Integer totalFinalPrice) {
+        TotalFinalPrice = totalFinalPrice;
+    }
+
+    public TotalFinalPrice() {
+
+    }
+
+    public Integer getTotalFinalPrice() {
+        return TotalFinalPrice;
+    }
+
+    public void setTotalFinalPrice(Integer totalFinalPrice) {
+        TotalFinalPrice = totalFinalPrice;
+    }
+}
