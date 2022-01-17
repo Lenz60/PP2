b632a589a26cf9f08e0543d9d4b7fe4a55e57613
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-17 17:01:14 +0700

    Testing Table

--- Admin/Web/test.php
+++ Admin/Web/test.php
@@ -1,37 +1,98 @@
-<?php 
-$table = array("2", "2","2", "9");
-
-if (stripos(json_encode($table),'1') !==FALSE){
-    $table1Status = 'green'; 
-}
-else {
-    $table1Status = "white";
-}
-if (stripos(json_encode($table),'2') !==FALSE){
-    $table2Status = 'green'; 
-}
-else {
-    $table2Status = "white";
-}
-
-if (stripos(json_encode($table),'9') !==FALSE){
-    $table3Status = 'green'; 
-}
-else {
-    $table3Status = "white";
-}
-
-
-
-
-
-
-echo $table1Status; 
-echo $table2Status;
-echo $table3Status;
-
-
-
-?>
-
-
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-0pky">Lokasi</th>
+    <th class="tg-0pky">Jumlah Pesanan</th>
+    <th class="tg-0pky">Nama Makanan</th>
+    <th class="tg-0pky">Tipe Makanan</th>
+    <th class="tg-0pky">Topping</th>
+    <th class="tg-0pky">Total Bayar</th>
+    <th class="tg-0pky">Status</th>
+    <th class="tg-0pky">Konfirmasi </th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax">
+        <table>
+            <tr>
+                <td>
+                    asd
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    sss
+                </td>
+            </tr>
+        </table>
+    </td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+    <td class="tg-0lax"></td>
+  </tr>
+</tbody>
+</table>
