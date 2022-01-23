e48b0bdcdb47d894349c73eff96f92752106fee4
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-23 19:33:12 +0700

    Test and developing purposes

--- Admin/Web/test.php
+++ Admin/Web/test.php
@@ -1,169 +1,54 @@
-<style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;}
-.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
-.tg .tg-0lax{text-align:left;vertical-align:top}
-</style>
-<style type="text/css">
-  .fa-1x {
-font-size: 1.5rem;
-}
-.navbar-toggler.toggler-example {
-cursor: pointer;
-}
-.dark-blue-text {
-color: #0A38F5;
-}
-.dark-pink-text {
-color: #AC003A;
-}
-.dark-amber-text {
-color: #ff6f00;
-}
-.dark-teal-text {
-color: #004d40;
-}
-</style>
+
 
 
 <!-- CSS -->
 <link rel="stylesheet" href="style.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
     <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
 
-    <!-- Sidenav -->
-<nav id="sidenav-1" class="sidenav" data-mdb-hidden="false">
-  <ul class="sidenav-menu">
-    <li class="sidenav-item">
-      <a class="sidenav-link">
-        <i class="far fa-smile fa-fw me-3"></i><span>Link 1</span></a>
-    </li>
-    <li class="sidenav-item">
-      <a class="sidenav-link"><i class="fas fa-grin fa-fw me-3"></i><span>Category 1</span></a>
-      <ul class="sidenav-collapse show">
-        <li class="sidenav-item">
-          <a class="sidenav-link">Link 2</a>
-        </li>
-        <li class="sidenav-item">
-          <a class="sidenav-link">Link 3</a>
-        </li>
-      </ul>
-    </li>
-    <li class="sidenav-item">
-      <a class="sidenav-link"><i class="fas fa-grin-wink fa-fw me-3"></i><span>Category
-          2</span></a>
-      <ul class="sidenav-collapse">
-        <li class="sidenav-item">
-          <a class="sidenav-link">Link 4</a>
-        </li>
-        <li class="sidenav-item">
-          <a class="sidenav-link">Link 5</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</nav>
-<!-- Sidenav -->
-
-<!-- Toggler -->
-<button data-mdb-toggle="sidenav" data-mdb-target="#sidenav-1" class="btn btn-primary"
-  aria-controls="#sidenav-1" aria-haspopup="true">
-  <i class="fas fa-bars"></i>
-</button>
-<!-- Toggler -->
-<table class="tg">
-<thead>
+<table class="table table-bordered border-dark">
   <tr>
-    <th class="tg-0pky">Lokasi</th>
-    <th class="tg-0pky">Jumlah Pesanan</th>
-    <th class="tg-0pky">Nama Makanan</th>
-    <th class="tg-0pky">Tipe Makanan</th>
-    <th class="tg-0pky">Topping</th>
-    <th class="tg-0pky">Total Bayar</th>
-    <th class="tg-0pky">Status</th>
-    <th class="tg-0pky">Konfirmasi </th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax">
-        <table>
-            <tr>
-                <td>
-                    asd
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    sss
-                </td>
-            </tr>
-        </table>
-    </td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
   </tr>
   <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <td>1</td>
   </tr>
   <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <td>1</td>
   </tr>
   <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <td>1</td>
   </tr>
   <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <td>1</td>
   </tr>
   <tr>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
-    <td class="tg-0lax"></td>
+    <td>1</td>
   </tr>
-</tbody>
+  <tr>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>1</td>
+  </tr>
 </table>
