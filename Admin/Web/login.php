9d0578729ace491290af348863dffa8d87a89a8a
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-01-24 15:51:17 +0700

    UI for Login is Done

--- Admin/Web/login.php
+++ Admin/Web/login.php
@@ -5,10 +5,13 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
         <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
         <title>Admin Pesan Palgading</title>
         <title>Login Admin Pesan Palgading</title>
     </head>
-    <body class="d-flex justify-content-center">
+    <body class="d-flex justify-content-center bgLogin" >
 
         <?php
         session_start();
@@ -21,6 +24,9 @@
             ?> 
             <div class="d-flex align-items-center" >
                 <fieldset class="border border-dark loginPadding" >
+                    <legend class="text-center" style="font-family: Ubuntu;"><p>Login Admin</p>
+                    <p>PesanPalgading</p>
+                    </legend>
                     <form action="login.php" method="POST">
                         <label class="form-label">Username</label>
                         <br>
