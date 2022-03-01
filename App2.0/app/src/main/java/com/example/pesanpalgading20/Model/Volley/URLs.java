2ac9e678cb742d93b9591e6b87644ca91a1b1ff3
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-03-01 20:20:04 +0700

    Changed Url from local database to Online database

--- App2.0/app/src/main/java/com/example/pesanpalgading20/Model/Volley/URLs.java
+++ App2.0/app/src/main/java/com/example/pesanpalgading20/Model/Volley/URLs.java
@@ -1,7 +1,7 @@
 package com.example.pesanpalgading20.Model.Volley;
 
 public class URLs {
-    private static final String BASE_URL = "https://192.168.8.108/Skripsi/AplhaLZProject/Api/";
+    private static final String BASE_URL = "https://pasarjasa-id.000webhostapp.com/api/";
     private static final String ROOT_URL = BASE_URL + "api.php?apicall=";
 
     public static final String GUEST_LOGIN = ROOT_URL + "loginguest";
